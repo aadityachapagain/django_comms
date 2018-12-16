3377f8cfae04107af8de15c2859c6c18c94cd17e
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-16 08:36:51 +0545

    use of decorator login required

--- boards/tests.py
+++ boards/tests.py
@@ -42,3 +42,15 @@ class BoardTopicTest(TestCase):
     def test_board_topic_url_resolves_topic_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func,board_topics)
+
+
+class LoginRequiredNewTopicTest(TestCase):
+
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+        self.url = reverse('new_topic', kwargs={'pk': 1})
+        self.response = self.client.get(self.url)
+
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(login_url=login_url, url=self.url))
